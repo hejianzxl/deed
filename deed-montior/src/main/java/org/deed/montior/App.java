e23af5e8305182a29131880aa4522decc205afc8
hejian <hejian@jk.cn>
2017-09-29 16:46:53 +0800

    多model

---
+++ deed-montior/src/main/java/org/deed/montior/App.java
@@ -0,0 +1,13 @@
+package org.deed.montior;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
