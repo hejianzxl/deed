32bcbf0a79474992025f4aa8f8fd4e6b696f8283
hejian <hejian@jk.cn>
2017-10-17 13:56:43 +0800

    构造InitializingBean

--- deed-core/src/main/java/org/deed/core/register/DefaultAbstractRegister.java
+++ deed-core/src/main/java/org/deed/core/register/DefaultAbstractRegister.java
@@ -4,28 +4,22 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.deed.client.server.annotation.RpcService;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class DefaultAbstractRegister extends Register implements ApplicationContextAware {
-	
-	private ApplicationContext applicationContext;
+public class DefaultAbstractRegister extends Register implements ApplicationContextAware, InitializingBean {
 
-	public static final Map<String, Object> REGISTERSERVICE = new ConcurrentHashMap<>(16);
-	
+	private ApplicationContext				applicationContext;
+
+	public static final Map<String, Object>	REGISTERSERVICE	= new ConcurrentHashMap<>(16);
+
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		//不兼容老版本spring2
 		this.applicationContext = applicationContext;
-		//处理annontation
-		Map<String, Object> rpcService = applicationContext.getBeansWithAnnotation(RpcService.class);
-		if(null!=rpcService && rpcService.size() > 0) {
-			rpcService.values().forEach((o) -> {
-				//简单缓存   后续可以使用模板方法子类实现发布RPC服务。支持多种nameServer
-				register(o);
-			});
-		}
 	}
-	
+
 	public Object getBean(Class<?> classzz) {
 		return applicationContext.getBean(classzz);
 	}
@@ -34,6 +28,18 @@ public class DefaultAbstractRegister extends Register implements ApplicationCont
 	public void register(Object o) {
 		System.out.println("发布RPC服务 " + o.getClass().getAnnotation(RpcService.class).value().getName());
 		REGISTERSERVICE.put(o.getClass().getAnnotation(RpcService.class).value().getName(), o);
-		//注册nameservice 服务列表
+		// 注册nameservice 服务列表
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// 处理annontation
+		Map<String, Object> rpcService = applicationContext.getBeansWithAnnotation(RpcService.class);
+		if (null != rpcService && rpcService.size() > 0) {
+			rpcService.values().forEach((o) -> {
+				// 简单缓存 后续可以使用模板方法子类实现发布RPC服务。支持多种nameServer
+				register(o);
+			});
+		}
 	}
 }
